0806cdbff39e332c77b119c8cb24aebfd0054b80
stelianrosca618 <stelianrosca618@outlook.com>
2023-12-12 20:36:14 +0200

    update my updates

--- js/tree.js
+++ js/tree.js
@@ -1,26 +1,36 @@
 
-import Treeselect from './treeselect-js.js';
+import Treeselect from '../lib/treeselect-js.js';
 
 let family_api_array=[];
-payload = {}
+let payload = {"data":"y"};
 let ajaxRequest = $.ajax({
     type: "POST",
-    url: getPredefinedApis,
-    data: JSON.stringify(payload),
+    url: "demo_test.txt",
+    data: payload,
     success: function (response) {
         if (response.length > 0) {
             let full_family_api_mapping_tmp=[];
-            for (let i = 0; i < response.length; i++) {
-                let apiId = response[i]["family_api_id"];
-                let family_id=response[i]["family_id"];
-                let family_value=response[i]["family_value"];
-                let api=response[i]["api_name"];
-                let family=response[i]["family"];
-                let isDefault=response[i]["isdefault"];
-                let f_item = {"family":family,"family_value":family_value,"apiId":apiId,"api":api,"isDefault":isDefault};
-                full_family_api_mapping_tmp.push(f_item);
-            }
-            console.log("----------- full_family_api_mapping_tmp ---- "+JSON.stringify(full_family_api_mapping_tmp));
+            // for (let i = 0; i < response.length; i++) {
+            //     let apiId = response[i]["family_api_id"];
+            //     let family_id=response[i]["family_id"];
+            //     let family_value=response[i]["family_value"];
+            //     let api=response[i]["api_name"];
+            //     let family=response[i]["family"];
+            //     let isDefault=response[i]["isdefault"];
+            //     let f_item = {"family":family,"family_value":family_value,"apiId":apiId,"api":api,"isDefault":isDefault};
+            //     full_family_api_mapping_tmp.push(f_item);
+            // }
+
+            full_family_api_mapping_tmp =
+            [
+            {"family":"AIML_Dialogflow","family_value":"aiml_dialogflow","apiId":1,"api":"aiml_api1","isDefault":"Y"},
+            {"family":"AIML_Dialogflow","family_value":"aiml_dialogflow","apiId":2,"api":"aiml_api2","isDefault":"Y"},
+            {"family":"AIML_Dialogflow","family_value":"aiml_dialogflow","apiId":3,"api":"aiml_api3","isDefault":"Y"},
+            {"family":"GSuite","family_value":"gsuite","apiId":4,"api":"gsuite_api1","isDefault":"Y"},
+            {"family":"GSuite","family_value":"gsuite","apiId":5,"api":"gsuite_api2","isDefault":"Y"},
+            {"family":"Others","family_value":"others","apiId":6,"api":"other_api1","isDefault":"Y"},
+            ];
+            //console.log("----------- full_family_api_mapping_tmp ---- "+JSON.stringify(full_family_api_mapping_tmp));
             family_api_array = full_family_api_mapping_tmp;
             assignvar();
         }
@@ -65,8 +75,25 @@ function makeOptions(apiArr){
 function assignvar(){
     const testOption = makeOptions(family_api_array);
     let defaultVals = [];
-    testOption[0].children.map(childItem => defaultVals.push(childItem.value));
-    console.log("testOption", testOption, defaultVals);
+    window.treeRelatinVals = [];
+    testOption[0].children.map(childItem =>{ 
+        defaultVals.push(childItem.value)
+        window.treeRelatinVals.push({
+            checked: true,
+            childOf: testOption[0].value,
+            hidden: true,
+            id: 1,
+            isClosed: false,
+            isGroup: false,
+            isPartialChecked: false,
+            level: 1,
+            name: childItem.value
+        })
+    });
+    // console.log("testOption", testOption, defaultVals);
+    window.treeValues = defaultVals;
+    
+    
     const slot = document.createElement('div')
     slot.innerHTML = '<a class="test" href="">Add new element</a>'
     const domEl = document.querySelector('.family_tree')
@@ -89,8 +116,12 @@ function assignvar(){
     treeselect.srcElement.addEventListener('input', (e) => {
         console.log("getting Child Values", treeselect.value);
         window.treeValues = treeselect.value;
-        console.log("Parent RelationData", e.detail);
+        console.log("Parent RelationData", e.detail, e);
         window.treeRelatinVals = e.detail;
     })
+
+    treeselect.srcElement.addEventListener('close', (e) => {
+        console.log('close', e);
+      })
 
 }
